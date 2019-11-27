449d7cbe3adc49b23633e96a13e1f068b8c4891a
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-11-27 16:17:15 +0700

    Adjust test to match with current implemented feature

--- backend/tests/unit/components/MapThumb.spec.js
+++ backend/tests/unit/components/MapThumb.spec.js
@@ -1,6 +1,6 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import MapThumb from '@/components/MapThumb'
-import ElementUI from 'element-ui';
+import ElementUI from 'element-ui'
 
 const localVue = createLocalVue()
 localVue.use(ElementUI)
@@ -16,11 +16,9 @@ describe('MapThumb', () => {
   it('renders a map', () => {
     const latitude = '-6.7'
     const longitude = '107.1'
-    const urlMap = `https://maps.google.com/maps?q=${latitude},${longitude}&t=&z=16&ie=UTF8&iwloc=&output=embed`
     const wrapper = factory({ latitude, longitude })
 
-    expect(wrapper.find('iframe').exists()).toBeTruthy()
-    expect(wrapper.find('iframe').attributes('src')).toBe(urlMap)
-    expect(wrapper.find('iframe').attributes('id')).toBe('gmap_canvas')
+    expect(wrapper.find('div').exists()).toBeTruthy()
+    expect(wrapper.find('div').attributes('id')).toBe('leafletmap')
   })
 })
